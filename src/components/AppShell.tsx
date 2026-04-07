aea7c3eddb3fd9e4175cac26f1668e764c07f6a6
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-07 15:47:29 -0300

    Update AppShell.tsx

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,1 +1,13 @@
+import Sidebar from './Sidebar'
+
+export default function AppShell({ children }: { children: React.ReactNode }) {
+  return (
+    <div style={{ display:'flex', minHeight:'100vh' }}>
+      <Sidebar />
+      <div style={{ flex:1, marginLeft:230, display:'flex', flexDirection:'column', minHeight:'100vh' }}>
+        {children}
+      </div>
+    </div>
+  )
+}
 
