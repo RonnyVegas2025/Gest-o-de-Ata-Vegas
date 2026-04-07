02f24af794c384dffa09b2c6866fbdf3e0ae8d36
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-07 15:53:57 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,23 +1,6 @@
-import { createBrowserClient, createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createBrowserClient } from '@supabase/ssr'
 
 const URL  = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const ANON = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createBrowserClient(URL, ANON)
-
-export function createServerSupabase() {
-  const cookieStore = cookies()
-  return createServerClient(URL, ANON, {
-    cookies: {
-      getAll: () => cookieStore.getAll(),
-      setAll: (list) => {
-        try {
-          list.forEach(({ name, value, options }) =>
-            cookieStore.set(name, value, options)
-          )
-        } catch {}
-      },
-    },
-  })
-}
